cd07a9d3f0224fdccfbece5b550a144a95d2341a
tom1dev <tdev429@aucklanduni.ac.nz>
2024-12-15 21:16:03 +1300

    added basic models for required attributes

--- backend/src/main/java/com/project_2/backend/models/UsersEventsModel.java
+++ backend/src/main/java/com/project_2/backend/models/UsersEventsModel.java
@@ -1,4 +1,44 @@
 package com.project_2.backend.models;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class UsersEventsModel {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private int userId;
+    private int eventId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getEventId() {
+        return eventId;
+    }
+
+    public void setEventId(int eventId) {
+        this.eventId = eventId;
+    }
+
+
+
 }
