c2c7a1dad02e2137bba78bc84e5f33f8d2aefb00
tom1dev <tdev429@aucklanduni.ac.nz>
2024-12-16 02:21:09 +1300

    added functionality to the Signin Controller

--- backend/src/main/java/com/project_2/backend/controllers/SignInController.java
+++ backend/src/main/java/com/project_2/backend/controllers/SignInController.java
@@ -1,4 +1,38 @@
 package com.project_2.backend.controllers;
 
+import com.project_2.backend.models.EventModel;
+import com.project_2.backend.services.EventService;
+import com.project_2.backend.services.SignInService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/event")
 public class SignInController {
+    @Autowired
+    private SignInService SignInService;
+
+    @PostMapping
+    public  ResponseEntity<String> signIn(@RequestBody Map<String, Object> payload) {
+        try{
+            String password = (String) payload.get("passwordHash");
+            String email = (String) payload.get("email");
+            if(SignInService.signIn(email,password)){
+                return new ResponseEntity<>("hihi", HttpStatus.OK);
+            }else{
+                return new ResponseEntity<>("SignInFailed", HttpStatus.UNAUTHORIZED);
+            }
+        }catch (Exception e){
+            return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST);
+
+        }
+    }
+
+
+
 }
