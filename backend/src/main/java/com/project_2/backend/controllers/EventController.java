5c726e2a40951bf3e8fa42179da56a02a382a64b
tom1dev <tdev429@aucklanduni.ac.nz>
2024-12-16 02:33:20 +1300

    added functionality to event controllers

--- backend/src/main/java/com/project_2/backend/controllers/EventController.java
+++ backend/src/main/java/com/project_2/backend/controllers/EventController.java
@@ -4,9 +4,8 @@ package com.project_2.backend.controllers;
 import com.project_2.backend.models.EventModel;
 import com.project_2.backend.services.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,9 +16,54 @@ public class EventController {
     private EventService eventService;
 
     @GetMapping
-    public List<EventModel> getAllEvents() {
-        return eventService.getAllEvents();
+    public ResponseEntity<List<EventModel>> getAllEvents() {
+        List<EventModel> events =  eventService.getAllEvents();
+        if(events != null){
+            return ResponseEntity.ok(events);
+
+        }else{
+            return ResponseEntity.status(404).body(null);
+        }
+
+
+
     }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventModel> getEventById(@PathVariable int id) {
+        EventModel event = eventService.getEventById(String.valueOf(id));
+        if(event != null){
+            return ResponseEntity.ok(event);
+        }else{
+            return ResponseEntity.status(404).body(null);
+        }
+    }
+
+    @GetMapping("/{id}/userAmount")
+    public ResponseEntity<String> getUserAmount(@PathVariable int id) {
+        try{
+            String amount = eventService.getUserAmountByEventId(String.valueOf(id));
+            if(amount != null){
+                return ResponseEntity.ok(amount);
+            }else{
+                return ResponseEntity.status(404).body(null);
+            }
+        }catch (Exception e){
+            return ResponseEntity.status(500).body(null);
+        }
+    }
+
+    @PostMapping("/")
+    public ResponseEntity<String> CreateEvent(@RequestBody EventModel event) {
+        try{
+            eventService.createEvent(event);
+            return ResponseEntity.status(201).body(null);
+        } catch (Exception e) {
+            return ResponseEntity.status(500).body(null);
+        }
+    }
+
 
 
 
