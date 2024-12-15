2cd9c746df360db94e9320f6b7f0cf5785bc4013
tom1dev <tdev429@aucklanduni.ac.nz>
2024-12-16 02:10:07 +1300

    added functionality to the sign in and user services

--- backend/src/main/java/com/project_2/backend/services/SignInService.java
+++ backend/src/main/java/com/project_2/backend/services/SignInService.java
@@ -1,4 +1,50 @@
 package com.project_2.backend.services;
 
+import com.project_2.backend.models.UserModel;
+import com.project_2.backend.repositories.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.Base64;
+
+import static java.awt.SystemColor.text;
+
+@Service
 public class SignInService {
+
+    private String hashPassword(String password) {
+        try{
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(hash);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public boolean signIn(String email, String password) {
+        try{
+            String passwordHash = hashPassword(password);
+
+            UserService userService = new UserService();
+            UserModel user  = userService.getUserByEmail(email);
+
+            if(user!= null && user.getPasswordHash().equals(passwordHash)) {
+                return true;
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return false;
+    }
+
+
+
+
+
 }
--- backend/src/main/java/com/project_2/backend/services/UserService.java
+++ backend/src/main/java/com/project_2/backend/services/UserService.java
@@ -1,4 +1,43 @@
 package com.project_2.backend.services;
 
+import com.project_2.backend.models.UserModel;
+import com.project_2.backend.repositories.EventRepository;
+import com.project_2.backend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository UserRepository;
+
+    public UserModel createUser(UserModel user) {
+        return UserRepository.save(user);
+    }
+
+    public List<UserModel> getAllUsers() {
+        return UserRepository.findAll();
+    }
+
+    public UserModel getUserById(String id) {
+        return UserRepository.findById(id).orElse(null);
+    }
+
+    public UserModel getUserByEmail(String email) {
+        return UserRepository.findByEmail(email);
+    }
+
+    public List<String> getAllUserEventIds(String id){
+        UsersEventsService usersEventsService = new UsersEventsService();
+
+        return usersEventsService.getUserEventIDs(id);
+    }
+
+    public UserModel updateUser(UserModel user) {
+        return UserRepository.save(user);
+
+    }
+
 }
