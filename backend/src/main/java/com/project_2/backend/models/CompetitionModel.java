3024072d37b88bdd433770a396d438c86ca2780d
tom1dev <tdev429@aucklanduni.ac.nz>
2024-12-21 00:35:22 +1300

    added a competition model

--- backend/src/main/java/com/project_2/backend/models/CompetitionModel.java
+++ backend/src/main/java/com/project_2/backend/models/CompetitionModel.java
@@ -1,4 +1,30 @@
 package com.project_2.backend.models;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Document
 public class CompetitionModel {
+    @Id
+    private String title;
+
+    private String[] QuestionIds;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String[] getQuestionIds() {
+        return QuestionIds;
+    }
+    public void setQuestionIds(String[] questionIds) {
+        QuestionIds = questionIds;
+    }
 }
--- backend/src/main/java/com/project_2/backend/repositories/CompetitionRepository.java
+++ backend/src/main/java/com/project_2/backend/repositories/CompetitionRepository.java
@@ -1,4 +1,8 @@
 package com.project_2.backend.repositories;
 
-public interface CompetitionRepository {
+import com.project_2.backend.models.CompetitionModel;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+
+public interface CompetitionRepository extends MongoRepository<CompetitionModel, String> {
 }
