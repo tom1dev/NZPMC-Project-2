7464c9d4b565291b26acee6901deecc7ba77c384
tom1dev <tdev429@aucklanduni.ac.nz>
2024-12-27 22:59:03 +1300

    added basic implementation for questions popup

--- frontend/src/components/competition/CompetitionTableEntry.jsx
+++ frontend/src/components/competition/CompetitionTableEntry.jsx
@@ -11,7 +11,7 @@ const CompetitionTableEntry = ({competition}) => {
     //enrolls the user to the event
     const togglePopup = (val, setter) => {
         console.log("togglePopup Clicked");
-        setPopupOpen(!popupOpen);
+        setter(!val);
     }
 
 
@@ -22,13 +22,14 @@ const CompetitionTableEntry = ({competition}) => {
             <div className={styles.eventTableListingBox}>
                 <h2 className={styles.eventName}>{competition.title}</h2>
                 <h2 className={styles.eventDate}>{competition.QuestionIds?competition.QuestionIds.size():0 }</h2>
-                <button className={styles.eventViewButton} onClick={(e) => {togglePopup(viewPopupOpen,setViewPopupOpen)}}>AddQuestion</button>
-                <button className={styles.eventViewButton} onClick={(e) => {togglePopup(questionPopupOpen,setQuestionPopupOpen)}}>View</button>
+
+                <button className={styles.eventViewButton} onClick={(e) => {togglePopup(questionPopupOpen,setQuestionPopupOpen)}}>AddQuestion</button>
+                <button className={styles.eventViewButton} onClick={(e) => {togglePopup(viewPopupOpen,setViewPopupOpen)}}>View</button>
             </div>
 
             {
                 questionPopupOpen &&
-                <AddQuestionPopup/>
+                <AddQuestionPopup setQuestionPopupOpen= {setQuestionPopupOpen}/>
 
             }
 
--- frontend/src/components/question/AddQuestionPopup.jsx
+++ frontend/src/components/question/AddQuestionPopup.jsx
@@ -1,8 +1,20 @@
-const AddQuestionPopup = () => {
-    return (
-        <div>
-            <h1>Add Question</h1>
+import style from '../../styles/UserPopup.module.css';
+import {useState,useEffect} from 'react';
+
+const AddQuestionPopup = ({setQuestionPopupOpen}) => {
+
+ return (
+        <div className={style.popupWindow}>
+            <div className={style.popupContainer}>
+                
+                    <h className={style.popupTitle}>Add Question</h>
+                    <h2 className={style.popupParram}>Competition Title:</h2>
+
+                    <button className={style.popupCloseButton} onClick={(e) => setQuestionPopupOpen(false)}>Close</button>
+            </div>
+
         </div>
+
     );
 }
 export default AddQuestionPopup;
