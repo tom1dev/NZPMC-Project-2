d56f40dfe7ce7d10df8de48c0834fd88e34093b3
tom1dev <tdev429@aucklanduni.ac.nz>
2025-01-11 11:22:01 +1300

    added a dropdown to display exsisting questions to the add question popup

--- frontend/src/components/question/AddQuestionPopup.jsx
+++ frontend/src/components/question/AddQuestionPopup.jsx
@@ -2,10 +2,22 @@ import style from '../../styles/UserPopup.module.css';
 import {useState,useEffect} from 'react';
 
 import competitionService from '../../services/competitionService';
+import Dropdown from '../misc/Dropdown';
 
 import CreateQuestion from './CreateQuestion';
+import QuestionDisplay from './QuestionDisplay';
 
 const AddQuestionPopup = ({competition,setQuestionPopupOpen}) => {
+    const [questions, setQuestions] = useState([]);
+
+
+    useEffect(() => {
+
+
+
+
+    },[]);
+
 
  return (
         <div className={style.popupWindow}>
@@ -13,6 +25,11 @@ const AddQuestionPopup = ({competition,setQuestionPopupOpen}) => {
                 
                     <h className={style.popupTitle}>Add Question</h>
                     <h2 className={style.popupParram}>Competition Title:{competition.title}</h2>
+                    
+                    <Dropdown DropdownTitle="Add An Exsisting Question">
+                                <QuestionDisplay questions={questions}/>
+                    </Dropdown>
+
 
                     <CreateQuestion competition={competition}/>
 
