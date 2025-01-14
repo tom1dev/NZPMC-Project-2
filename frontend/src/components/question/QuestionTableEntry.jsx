49423bc940bf8df24cae9b2f8e60536242ae091a
tom1dev <tdev429@aucklanduni.ac.nz>
2025-01-15 02:21:36 +1300

    the question view now displays the difficulty and the topic

--- frontend/src/components/question/QuestionTableEntry.jsx
+++ frontend/src/components/question/QuestionTableEntry.jsx
@@ -4,6 +4,8 @@ const QuestionTableEntry = ({ question }) => {
     return (
         <div className={styles.questionContainer}>
             <h2 className={styles.questionParram}>Title: {question.title}</h2>
+            <h2 className={styles.questionParram}>Difficulty: {question.difficulty}</h2>
+            <h2 className={styles.questionParram}>Topic: {question.topic}</h2>
             <h2 className={styles.questionParram}>Option 1: {question.options[0]}</h2>
             <h2 className={styles.questionParram}>Option 2: {question.options[1]}</h2>
             <h2 className={styles.questionParram}>Option 3: {question.options[2]}</h2>
