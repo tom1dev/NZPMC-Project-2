0d279044c494723dbded5d7b5e1041d3b067cea8
tom1dev <tdev429@aucklanduni.ac.nz>
2025-01-15 13:56:53 +1300

    added date, endtome and start time to the competitionmodel

--- backend/src/main/java/com/project_2/backend/models/CompetitionModel.java
+++ backend/src/main/java/com/project_2/backend/models/CompetitionModel.java
@@ -3,13 +3,50 @@ package com.project_2.backend.models;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 
 @Document
 public class CompetitionModel {
     @Id
     private String title;
+
+    private Date date;
+
+    private LocalTime startTime;
+    private LocalTime endTime;
+
+    public String getStartTime() {
+        return getStringFromTime(startTime);
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = getTimeFromString(startTime);
+    }
+
+    public String getEndTime() {
+        return getStringFromTime(endTime);
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = getTimeFromString(endTime);
+    }
+
+    public LocalTime getTimeFromString(String time){
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return LocalTime.parse(time,formatter);
+
+    }
+
+    public String getStringFromTime(LocalTime time){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        return time.format(formatter);
+    }
 
     private String[] QuestionIds;
 
@@ -26,5 +63,26 @@ public class CompetitionModel {
     }
     public void setQuestionIds(String[] questionIds) {
         QuestionIds = questionIds;
+    }
+
+    public String getDate() {
+        if(date != null){
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            String formattedDate = formatter.format(date);
+            return formattedDate;
+        } else{
+            return null;
+        }
+
+    }
+
+    public void setDate(String date) {
+        try{
+            SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+            this.date = formatter.parse(date);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
