a5685c084356adf5797ddcb4fe8f63013401b282
tom1dev <tdev429@aucklanduni.ac.nz>
2025-01-16 06:18:02 +1300

    changed styling around competing in a competition

--- frontend/src/components/competition/AnswerInput.jsx
+++ frontend/src/components/competition/AnswerInput.jsx
@@ -30,7 +30,7 @@ const AnswerInput = ({question, setAnswer}) =>{
             <h3 className={style.answerTitle}>Answer: </h3>
 
             {/* SHOWS THE  opitions for the answers*/}
-            <select value={selectedOption} onChange={handleChange}>
+            <select className= {style.selecter} value={selectedOption} onChange={handleChange}>
                 <option value="1">{question.options[0]}</option>
                 <option value="2">{question.options[1]}</option>
                 <option value="3">{question.options[2]}</option>
