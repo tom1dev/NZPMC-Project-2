b7b7dc88dd541f0de36ea14e30b6ff3ecc9013bb
tom1dev <tdev429@aucklanduni.ac.nz>
2025-01-16 16:05:04 +1300

    added metrics to the generated result

--- frontend/src/components/competition/GeneratedResults.jsx
+++ frontend/src/components/competition/GeneratedResults.jsx
@@ -3,37 +3,92 @@ import {useState, useEffect} from 'react'
 import attemptService from '../../services/attemptService';
 import UserResult from './UserResult';
 import competitionService from '../../services/competitionService';
+import Dropdown from '../misc/Dropdown';
+import { use } from 'react';
+import ResultsMetrics from './ResultsMetrics';
 
 const GeneratedResults = ({togglePopup, competition}) => {
     const[attempts, setAttempts] = useState([]);
     const[questions, setQuestionsAtempts] = useState([]);
 
+    const[questionScoreMap, setQuestionScoreMap] = useState(new Map([]));
+    const[userScoreMap, setUserScoreMap] = useState(new Map([]));
+
+
     //gets the attempts and the questions for the competition
     useEffect(() => {
-        const getAttempts = async () => {
-            try {    
-                const gottenAtempts = await attemptService.getAllCompetitionEvents(competition.title);
-                
-                setAttempts(gottenAtempts)
-            }catch(error){
-                console.log(error)
-            }
-        }
 
-        const getQuestionAnswers = async () => {
-            try {    
-                const gottenQuestions = await competitionService.getQuestionsForCompetition(competition.title);
-                setQuestionsAtempts(gottenQuestions);
-                
-            }catch(error){
-                console.log(error)
-            }
-        }
 
         getQuestionAnswers();
         getAttempts();
+        
 
     },[]);
+
+    //calculates the score for the user and the questions
+    useEffect(() => {
+
+
+        calculateScore();
+    },[attempts,questions]);
+
+    const calculateScore = () => {
+        const newQuestionScoreMap = new Map(questionScoreMap);
+        const newUserScoreMap = new Map(userScoreMap);
+
+
+
+        attempts.forEach((attempt) => {
+                let amountRight = 0;
+                //if the attempt has attempts and questions are not empty then check if the user got the question right
+                if(attempt.attempts && questions && questions.length >0){
+                    Object.entries(attempt.attempts).forEach(([key, value]) => {
+                        questions.forEach((question) =>{
+                            
+                            //if the question title matches and correct choice chosen update the score maps
+                            if(question.title === key && question.correctChoiceIndex+1 == value){
+                                amountRight = amountRight+1;
+                                if (newQuestionScoreMap.get(question.title)){
+                                    newQuestionScoreMap.set(question.title,newQuestionScoreMap.get(question.title)+1);
+                                }else{
+                                    newQuestionScoreMap.set(question.title,1);
+                                }
+
+                            }
+        
+                        })
+                    });
+                }
+                
+                newUserScoreMap.set(attempt.studentEmail,amountRight);
+        });
+
+        setQuestionScoreMap(newQuestionScoreMap);
+        setUserScoreMap(newUserScoreMap);
+    }
+
+
+    const getAttempts = async () => {
+        try {    
+            const gottenAtempts = await attemptService.getAllCompetitionEvents(competition.title);
+            
+            setAttempts(gottenAtempts)
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+    const getQuestionAnswers = async () => {
+        try {    
+            const gottenQuestions = await competitionService.getQuestionsForCompetition(competition.title);
+            setQuestionsAtempts(gottenQuestions);
+            
+        }catch(error){
+            console.log(error)
+        }
+    }
+
+
 
     return (
         <div className={style.popupWindow}>
@@ -41,9 +96,16 @@ const GeneratedResults = ({togglePopup, competition}) => {
 
                     <h2 className={style.popupParram}>Generated Results</h2>       
                     <div className={style.resultContainer}>
-                        {(questions.length>0&& attempts.length>0)? attempts.map((attempt,index) => {return <UserResult key={index}  attempt = {attempt} questions = {questions}/>}):
-                        
-                            <h2 className={style.popupParram}>No Results</h2>}
+                        <ResultsMetrics questions={questions} questionScoreMap={questionScoreMap} userScoreMap={userScoreMap}/>
+
+
+
+                        <Dropdown DropdownTitle="User Results ">
+                            {(questions.length>0&& attempts.length>0)? attempts.map((attempt,index) => {
+                                return <UserResult key={index}  attempt = {attempt} questions = {questions} amountRight={userScoreMap.get(attempt.studentEmail)}/>}):
+                            
+                                <h2 className={style.popupParram}>No Results</h2>}
+                        </Dropdown>
                     </div>    
                       
                     <button className={style.popupCloseButton} onClick={(e) => togglePopup()}>Close</button>
---
+++ frontend/src/components/competition/ResultsMetrics.jsx
@@ -0,0 +1,155 @@
+import styles from "../../styles/competitionResults.module.css"
+import {useState, useEffect} from 'react';
+import { BarChart } from '@mui/x-charts/BarChart'; 
+import { use } from "react";
+
+const ResultsMetrics = ({ questions, questionScoreMap ,userScoreMap }) => {
+    const[isUserResultsGraph, setIsUserResultsGraph] = useState(false);
+    const[textMetrics, setTextMetrics] = useState({
+        mean: 0,
+        median: 0,
+        high: 0,
+        low: 0
+    });
+
+    useEffect(() => {
+        calculateTextMetrics();
+    },[userScoreMap]);
+
+
+    const calculateTextMetrics = () => {
+
+        let newTextMetrics = {
+            mean: 0,
+            median: 0,
+            high: 0,
+            low: 0
+        }
+
+
+
+        userScoreMap.values().forEach((value) => {
+            newTextMetrics.mean += value/questions.length;
+        });
+
+        newTextMetrics.mean = newTextMetrics.mean/userScoreMap.size;
+
+        let sortedValues = Array.from(userScoreMap.values()).sort((a,b) => a-b);
+        newTextMetrics.median = sortedValues[Math.floor(sortedValues.length/2)]/questions.length;
+        
+
+        newTextMetrics.high = sortedValues[sortedValues.length-1];
+        newTextMetrics.low = sortedValues[0];
+
+
+        setTextMetrics(newTextMetrics);
+
+
+    }
+
+    const calculateUserResultsByQuad = (quadAmount) => {
+        let data = [];
+        for(let i = 0; i<quadAmount; i++){
+            data.push(0);
+        }
+
+
+        let quadSize = 100/quadAmount;
+
+        userScoreMap.values().forEach((value) => {
+            const userPercentage = value/questions.length * 100;
+            let quadIndex = Math.floor(userPercentage/quadSize);
+
+            if(quadIndex === quadAmount){
+                quadIndex = quadAmount-1;
+            }
+            data[quadIndex] = data[quadIndex] + 1;
+            
+                
+            }
+        );
+
+        return data;
+    };
+
+   
+
+
+
+        return(
+        <div className = {styles.resultsMetricsContainer}> 
+            <h1 className = {styles.title}>Results Metrics</h1>
+
+
+            {isUserResultsGraph ? 
+            <>
+                <h2 className = {styles.graphTitle}>User Results Graph</h2>
+                <div className = {styles.graphContainer}>
+                    
+                    <BarChart
+                        xAxis={[
+                            {
+                            id: 'barCategories',
+                            data: ['0-20%', '20-40%', '40-60%', '60-80%', '80-100%'],
+                            scaleType: 'band',
+                            },
+                        ]}
+                        series={[
+                            {
+                            data: calculateUserResultsByQuad(5),
+                            },
+                        ]}
+                    />
+                </div>
+            </>:
+            <> 
+            
+            
+                <h2 className = {styles.graphTitle}>Question Results Graph</h2>
+                    {console.log(questionScoreMap)}
+                <div className = {styles.graphContainer}>
+                    <BarChart
+                        xAxis={[
+                            {
+                            id: 'barCategories',
+                            data: Array.from(questionScoreMap.keys()),
+                            scaleType: 'band',
+                            },
+                        ]}
+                        series={[
+                            {
+                            data: Array.from(questionScoreMap.values()),
+                            },
+                        ]}
+                    />
+                </div>
+            
+            </>}
+            
+            <button className = {styles.toggleButton} onClick = {() => setIsUserResultsGraph(!isUserResultsGraph)}>{isUserResultsGraph ? "Show Question Results Graph" : "Show User Results Graph"}</button>
+
+
+
+            <div className = {styles.textMetricsContainer}>
+
+                <h2 className = {styles.textMetric}>Mean: {textMetrics.mean} </h2>
+                <h2 className = {styles.textMetric}>Median: {textMetrics.median}</h2>
+                <h2 className = {styles.textMetric}>High: {textMetrics.high }</h2>
+                <h2 className = {styles.textMetric}>Low: {textMetrics.low}</h2>
+                <h2 className = {styles.textMetric}>Amount of Participants: {userScoreMap.size}</h2>
+
+
+
+
+            </div>
+
+
+
+
+
+                        
+        </div>);
+}
+
+export default ResultsMetrics;
+
